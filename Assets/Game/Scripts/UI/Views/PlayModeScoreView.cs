2d8d5a36e3293627f4d9d4a75544a3cb6edec0e7
Barmaglot-is-here <98582951+Barmaglot-is-here@users.noreply.github.com>
2025-03-12 14:09:56 +0300

    - Реализовал PlayModeScoreView (отрисовывает значение рекорда, используя отдельные спрайты вместо шрифта)

--- Assets/Game/Scripts/UI/Views/PlayModeScoreView.cs
+++ Assets/Game/Scripts/UI/Views/PlayModeScoreView.cs
@@ -1,13 +1,91 @@
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayModeScoreView : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _score;
+    private Transform _container;
+
+    [SerializeField]
+    private GameObject _defaultInstance;
+    [SerializeField]
+    private Sprite[] _spritePrefabs;
+
+    [SerializeField]
+    private float _digitsSpace;
+
+    private List<SpriteRenderer> _spriteInstances;
+
+    public void Awake()
+    {
+        _spriteInstances    = new();
+
+        var firstSprite = _defaultInstance.GetComponent<SpriteRenderer>();
+        _spriteInstances.Add(firstSprite);
+    }
 
     public void Show(int score)
     {
-        Debug.Log(score);
+        int i = 1;
+        foreach (int spriteIndex in GetDigits(score))
+        {
+            if (_spriteInstances.Count < i)
+            {
+                var sprite      = AddNew(_defaultInstance);
+
+                _spriteInstances.Add(sprite);
+            }
+
+            _spriteInstances[^i].sprite = _spritePrefabs[spriteIndex];
+
+            i++;
+        }
+
+        ResizeView();
+    }
+
+    private IEnumerable<int> GetDigits(int number)
+    {
+        for (; number != 0;)
+        {
+            int digit = number / 10;
+
+            int spriteIndex = number - digit * 10;
+
+            number = digit;
+
+            yield return spriteIndex;
+        }
+    }
+
+    private SpriteRenderer AddNew(GameObject prefab)
+    {
+        var instance = Instantiate(prefab, _container);
+
+        return instance.GetComponent<SpriteRenderer>();
+    }
+
+    private void ResizeView()
+    {
+        float totalWidth = 0;
+
+        foreach (var sprite in _spriteInstances)
+        {
+            sprite.transform.localPosition = new (totalWidth, 0);
+
+            totalWidth += sprite.size.x + _digitsSpace;
+        }
+
+        CenterContainer();
+    }
+
+    private void CenterContainer()
+    {
+        float lastSpritePosition = _spriteInstances[^1].transform.localPosition.x;
+        float containerXPosition = -lastSpritePosition / 2 * _container.localScale.x;
+
+        _container.localPosition = new(containerXPosition,
+                                       _container.localPosition.y,
+                                       _container.localPosition.z);
     }
 }
